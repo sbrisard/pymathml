05728b7ca26881ec0569a95d92e34b1450bf3323
Sébastien Brisard <sebastien.brisard@m4x.org>
2017-12-16 06:16:11 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,34 @@
+import re
+
+from setuptools import setup
+
+with open('pymathml/pymathml.py', 'r') as f:
+    lines = f.read()
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+                        lines, re.MULTILINE).group(1)
+    description = re.search(r'^u\"\"\"(.*)',
+                            lines, re.MULTILINE).group(1)
+    long_description = re.search('^u\"\"\"(.*)^\"\"\"',
+                                 lines, re.MULTILINE | re.DOTALL).group(1)
+    author = re.search(r'^__author__\s*=\s*[\'"]([^\'"]*)[\'"]',
+                       lines, re.MULTILINE).group(1)
+
+setup(
+    name='pymathml',
+    version=version,
+    description=description,
+    long_description=long_description,
+    url='',
+    author=author,
+    author_email='',
+    py_modules=['pymathml'],
+    license='BSD-3',
+    classifiers=['Development Status :: 4 - Beta',
+                 'Intended Audience :: Developers',
+                 'Intended Audience :: Science/Research',
+                 'License :: OSI Approved :: BSD License',
+                 'Operating System :: OS Independent',
+                 'Topic :: Software Development :: Build Tools',
+                 'Programming Language :: Python :: 3',
+                 'Topic :: Scientific/Engineering'],
+)
