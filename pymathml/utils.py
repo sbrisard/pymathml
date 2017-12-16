5d7e289abe9b741d1735665ae63844a8a0a41e40
Sébastien Brisard <sebastien.brisard@m4x.org>
2017-12-16 07:06:21 +0100

    Fixed imports

--- pymathml/utils.py
+++ pymathml/utils.py
@@ -1,6 +1,8 @@
 """A collection of functions to facilitate creation of expressions.
 
 """
+from core import Operator, Table, TableRow, TableEntry, Under
+
 
 def table(rows, **attributes):
     return Table(*[TableRow(*[TableEntry(e) for e in r]) for r in rows],
