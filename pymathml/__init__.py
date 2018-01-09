8932e5256cf94914e97264978474021fa9740d4b
Sébastien Brisard <sebastien.brisard@m4x.org>
2018-01-09 08:17:45 +0100

    Moved comments to documentation

--- pymathml/__init__.py
+++ pymathml/__init__.py
@@ -1,19 +1,4 @@
 """Create MathML expressions programatically with Python.
-
-One notable exception is automatic parenthetizing, which is *not*
-implemented. Therefore, this code:
-
-    a = Identifier('a')
-    b = Identifier('b')
-    (a+b)**2
-
-would be translated to the following MathML
-
-    <mrow><mi>a</mi><mo>+</mo><msup><mi>b</mi><mn>2</mn></msup></mrow>
-
-which in fact renders as
-
-    a+b²
 """
 
 __author__ = 'Sebastien Brisard'
